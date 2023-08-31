97f1615828de18ca1020689e186b57f6d229e2e4
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 23:47:37 +0530

    First Commit

---
+++ src/screens/RegistrationScreen.tsx
@@ -0,0 +1,65 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const RegistrationScreen: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegistration = () => {
+    // Perform registration API call here
+    // Use the UserApi.registerUser() function with the provided request body
+
+    // Example API call:
+    // UserApi.registerUser({ name, email, password })
+    //   .then((response) => {
+    //     // Handle successful registration
+    //   })
+    //   .catch((error) => {
+    //     // Handle registration error
+    //   });
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Register" onPress={handleRegistration} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
+
+export default RegistrationScreen;
